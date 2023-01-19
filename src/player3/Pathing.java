973a97f94ad762ca3a70bd695838ee567739d7a2
Daniel Brown <dtbrown@mit.edu>
2023-01-18 21:29:34 -0500

    Added saving path functionality for bots, need to work on obstacle collision handling

--- src/player3/Pathing.java
+++ src/player3/Pathing.java
@@ -12,7 +12,37 @@ public class Pathing {
     // Basic bug nav - Bug 0
 
     static Direction currentDirection = null;
+    static Path currentPath = null;
+    static void bugZero(RobotController rc, MapLocation target) throws GameActionException {
+        if (rc.getLocation().equals(target)) {
+            return;
+        }
+        if (!rc.isActionReady()) {
+            return;
+        }
+        Direction d = rc.getLocation().directionTo(target);
+        if (rc.canMove(d)) {
+            rc.move(d);
+            currentDirection = null; // there is no obstacle we're going around
+        } else {
+            // Going around some obstacle: can't move towards d because there's an obstacle there
+            // Idea: keep the obstacle on our right hand
 
+            if (currentDirection == null) {
+                currentDirection = d;
+            }
+            // Try to move in a way that keeps the obstacle on our right
+            for (int i = 0; i < 8; i++) {
+                if (rc.canMove(currentDirection)) {
+                    rc.move(currentDirection);
+                    currentDirection = currentDirection.rotateRight();
+                    break;
+                } else {
+                    currentDirection = currentDirection.rotateLeft();
+                }
+            }
+        }
+    }
     static void moveTowards(RobotController rc, MapLocation target) throws GameActionException {
         if (rc.getLocation().equals(target)) {
             return;
@@ -21,15 +51,17 @@ public class Pathing {
             return;
         }
         System.out.println("" + rc.getLocation()+ ", " +  closetToTarget(rc, target));
-        ArrayList<MapLocation> path = pathing(rc.getLocation(), closetToTarget(rc, target), rc);
-        if (path == null || path.size() == 0) {
+        if (currentPath == null) {
+            currentPath = new Path(pathing(rc.getLocation(), closetToTarget(rc, target), rc), rc);
+        }
+        if (currentPath == null) {
+            bugZero(rc, target);
+        } else if (currentPath.moveOnPath()) {
             return;
+        } else {
+            bugZero(rc, target);
         }
-        currentDirection = rc.getLocation().directionTo(path.get(0));
 
-        if (rc.canMove(currentDirection)) {
-            rc.move(currentDirection);
-        }
         
         // Direction d = rc.getLocation().directionTo(target);
         // if (rc.canMove(d)) {
